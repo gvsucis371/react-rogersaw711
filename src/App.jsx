4f33a8248aed76d33b1cbf4c86ad3f619cdecebf
rogersaw711 <127700260+rogersaw711@users.noreply.github.com>
2025-03-24 22:27:20 -0400

    [GRADE ME, PART 2]
    Made a few changes to the view to reduce some slop, such as removing the trailing "," from the actors list. Added CRUD functionality based on the ideas from the Color2 Creator thing shown in class.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
-import './App.css'
-//Note: Reviews are AI generated. As they are AI generated, they may or may not be real reviews.
-function App() {
-  const data =[
+import React, { useState } from "react";
+import "./App.css";
+
+function Review({ name }) {
+  return <li>{name}</li>;
+}
+
+function ReviewList({ reviews }) {
+  return (
+    <ul class="reviews">
+      {reviews.map((review, index) => (
+        <Review name={review} key={index} />
+      ))}
+    </ul>
+  );
+}
+
+function Actors({ actors }) {
+  //This time around I realized we can .join() instead of having that ugly trailing comma
+  return (
+    <div class="actors">
+      <h3>Starring:</h3>
+      <p>{actors.join(", ")}</p>
+    </div>
+  );
+}
+
+function Movie({ name, actors, reviews, onDelete, onEdit }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <Actors actors={actors} />
+      <ReviewList reviews={reviews} />
+      <button onClick={onDelete}>Delete</button>
+      <button onClick={onEdit}>Edit</button>
+    </div>
+  );
+}
+
+function Movies() {
+  const [movies, setMovies] = useState([
     {
       name: "Ratatouille",
       actors: ["Patton Oswalt", "Ian Holm", "Lou Romano", "Brian Dennehy"],
@@ -29,49 +66,95 @@ function App() {
         "Gollum's character development is truly groundbreaking."
       ]
     }
-  ];
+  ]);
 
-function Review(props) {
-    return <li> {props.name} </li>;
-}
+  const [newMovie, setNewMovie] = useState({ name: "", actors: "", reviews: "" });
+  const [editingIndex, setEditingIndex] = useState(null);
 
-function ReviewList(props) {
-    return <ul className="reviews" > {
-        props.reviews.map((item, index) => (
-            <Review name={item} key={index} />
+  //If they're editing we've gotta populate the field up top like demonstrated in class
+  //There's a big issue that I didn't see coming with the lists of elements. As a stopgap we can just split with " | "
+  // This means we have to map those when populating the list too and when parsing out what they put in
+  const handleAddMovie = () => {
+    if (newMovie.name && newMovie.actors && newMovie.reviews) {
+      if (editingIndex !== null) {
+        const updatedMovies = [...movies];
+        updatedMovies[editingIndex] = {
+          name: newMovie.name,
+          actors: newMovie.actors.split(" | "),
+          reviews: newMovie.reviews.split(" | ")
+        };
+        setMovies(updatedMovies);
+        setEditingIndex(null);
+      } else {
+        setMovies([...movies, {
+          name: newMovie.name,
+          actors: newMovie.actors.split(" | "),
+          reviews: newMovie.reviews.split(" | ")
+        }]);
+      }
+      setNewMovie({ name: "", actors: "", reviews: "" });
+    }
+  };
+
+  const handleDeleteMovie = (index) => {
+    setMovies(movies.filter((_, i) => i !== index));
+  };
+
+  const handleEditMovie = (index) => {
+    const movie = movies[index];
+    setNewMovie({
+      name: movie.name,
+      actors: movie.actors.join(" | "),
+      reviews: movie.reviews.join(" | ")
+    });
+    setEditingIndex(index);
+  };
+
+  //Use the model shown in class where you swap the add form to an edit form on button click (The color2 example)
+  return (
+    <section>
+      <div class="add-movie-form">
+        <h2>{editingIndex !== null ? "Edit Movie" : "Add a New Movie"}</h2>
+        <em>Separate list values with " | "</em>
+        <br />
+        <input
+          type="text"
+          placeholder="Movie Name"
+          value={newMovie.name}
+          onChange={(e) => setNewMovie({ ...newMovie, name: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Actors"
+          value={newMovie.actors}
+          onChange={(e) => setNewMovie({ ...newMovie, actors: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Reviews"
+          value={newMovie.reviews}
+          onChange={(e) => setNewMovie({ ...newMovie, reviews: e.target.value })}
+        />
+        <button onClick={handleAddMovie}>{editingIndex !== null ? "Update Movie" : "Add Movie"}</button>
+      </div>
+      <div class="movies">
+        {movies.map((movie, index) => (
+          <Movie
+            key={index}
+            name={movie.name}
+            actors={movie.actors}
+            reviews={movie.reviews}
+            onDelete={() => handleDeleteMovie(index)}
+            onEdit={() => handleEditMovie(index)}
+          />
         ))}
-    </ul>
+      </div>
+    </section>
+  );
 }
 
-function Actors(props) {
-    return <div className='actors'>
-        <h3>Starring:</h3>
-        <p>
-        {props.actors.map((actor, index) => (`${actor}, `))}
-        </p>
-    </div>;
+function App() {
+  return <Movies />;
 }
 
-function Movie(props) {
-    return <div>
-        <h2 onClick={() => console.log(`You clicked on movie ${props.name}`)}> {props.name} </h2>
-        <Actors actors={props.actors} />
-        <ReviewList reviews={props.reviews} />
-    </div>;
-}
-
-function Movies(data) {
-  console.log(data);
-    return <section>
-        <div className='movies' >
-          {data.map(function (movie, index){
-            return <Movie key={index} name={movie.name} actors={movie.actors} reviews={movie.reviews} />
-        })}
-        </div>
-    </section>;
-}
-
-return Movies(data)
-}
-
-export default App
+export default App;
